0f0b49d4997bfecad37ff93b6b498525feb4727c
Siddharth Saxena <sidd.saxena96@gmail.com>
2019-10-01 22:36:07 +0530

    First push - Project Files

---
+++ Assets/Scripts/AnimationControllers/AnimationHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AnimationHandler : MonoBehaviour
+{
+    [SerializeField] private Animator animator = null;
+
+    private void Awake()
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+    }
+
+    public virtual void OnReceiveEvent(bool eventInput) { }
+    public virtual void OnReceiveEvent(int eventInput) { }
+    public virtual void SetBool(string boolName, bool boolStatus)
+    {
+        animator.SetBool(boolName,boolStatus);
+    }
+}
---
+++ Assets/Scripts/AnimationControllers/RightSmokingHandAnimationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RightSmokingHandAnimationController : AnimationHandler
+{
+    [SerializeField] private Animator cigaretteAnimator = null;
+
+    public override void OnReceiveEvent(int eventInput)
+    {
+        if(eventInput == 0){
+            cigaretteAnimator.SetBool("DragOn",true);
+        }
+
+        else{
+            cigaretteAnimator.SetBool("DragOn",false);
+            Debug.Log("Here");
+        }
+    }
+}
