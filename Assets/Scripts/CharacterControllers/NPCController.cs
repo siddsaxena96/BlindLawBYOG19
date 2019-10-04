5677ba03015e70fb4a1d5d219e63bd418fc07d99
Siddharth Saxena <sidd.saxena96@gmail.com>
2019-10-04 14:07:01 +0530

    +LevelControllerAdded +AnimationControllers updated

---
+++ Assets/Scripts/CharacterControllers/NPCController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NPCController : MonoBehaviour
+{
+    [SerializeField] private AnimationHandler animationHandler = null;
+    [SerializeField] private SpriteRenderer spriteRenderer = null;
+    [SerializeField] private float movementSpeed = 5f;
+    public bool isWalking = false;
+
+    public void FlipSpriteX()
+    {
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+    }
+
+    public void FlipSpriteY()
+    {
+        spriteRenderer.flipY = !spriteRenderer.flipY;
+    }
+
+    public void OnWalkTo(Transform destination)
+    {
+        StartCoroutine(WalkingCoroutine(destination.position));
+    }
+
+    public void OnSit()
+    {
+        animationHandler.animator.SetBool("SitOn", true);
+    }
+
+    public void OnStand()
+    {
+        animationHandler.animator.SetBool("SitOn", false);
+    }
+
+    IEnumerator WalkingCoroutine(Vector3 destination)
+    {
+        isWalking = true;
+        animationHandler.animator.SetBool("WalkOn", true);
+        while (Vector2.Distance(transform.position, destination) > 0.5f)
+        {
+            transform.position = Vector2.Lerp(transform.position, destination, movementSpeed);
+            yield return new WaitForSeconds(0.1f);
+        }
+        isWalking = false;
+        animationHandler.animator.SetBool("WalkOn", false);
+    }
+}
--- Assets/Scripts/LevelControllers/Level1Chamber.cs
+++ Assets/Scripts/LevelControllers/Level1Chamber.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Level1Chamber : MonoBehaviour,ILevelController
+public class Level1Chamber : MonoBehaviour, ILevelController
 {
     [SerializeField] private AnimationHandler smokingHand = null;
-    [SerializeField] private UnderStudyController underStudyController = null;
+    [SerializeField] private NPCController underStudyController = null;
+    [SerializeField] private NPCController client1 = null;
+    [SerializeField] private Transform tableStandingPoint = null;
+    [SerializeField] private Transform wallStandingPoint = null;
+    [SerializeField] private Transform chairLeft = null;
+    [SerializeField] private Transform chairRight = null;
 
-    private void Start() {
+    private void Start()
+    {
         StartLevel();
     }
 
@@ -19,7 +25,26 @@ public class Level1Chamber : MonoBehaviour,ILevelController
     IEnumerator LevelCoroutine()
     {
         yield return new WaitForSeconds(2);
-        smokingHand.animator.SetBool("TakeDrag",true);
-        underStudyController.WalkToChamberStandingPoint();
+        smokingHand.animator.SetBool("TakeDrag", true);
+        underStudyController.OnWalkTo(tableStandingPoint);
+        while (underStudyController.isWalking)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return new WaitForSeconds(3);
+        Debug.Log("Second Walk");
+        underStudyController.OnWalkTo(wallStandingPoint);
+        client1.OnWalkTo(chairLeft);
+        while (underStudyController.isWalking)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        Debug.Log("Flip Now");
+        underStudyController.FlipSpriteX();
+        while (client1.isWalking)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        client1.OnSit();
     }
 }
