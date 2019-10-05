46130ea508ecfc0d2353c698ff4a97f0853ecb23
Siddharth Saxena <sidd.saxena96@gmail.com>
2019-10-05 22:40:40 +0530

    +Street scene added with blueprint +PlayerController added +PhysicsObject - for handling movement physics

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform player = null;
+    [SerializeField] private float clampMinX = 0;
+    [SerializeField] private float clampMaxX = 25;
+    [SerializeField] private float yLevel = 0;
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, clampMinX, clampMaxX), yLevel, transform.position.z);
+        Debug.Log(transform.position);
+    }
+    private float shakeAmount = 0f;
+
+    public void Shake(float amount, float length)
+    {
+        shakeAmount = amount;
+        InvokeRepeating("DoShake", 0, 0.01f);
+        Invoke("StopShake", length);
+    }
+
+    private void DoShake()
+    {
+        Vector3 camPos = transform.position;
+        float offsetX = Random.value * shakeAmount * 2 - shakeAmount;
+        float offsetY = Random.value * shakeAmount * 2 - shakeAmount;
+        camPos.x += offsetX;
+        camPos.y += offsetY;
+        transform.position = camPos;
+    }
+
+    private void StopShake()
+    {
+        CancelInvoke("DoShake");
+        transform.localPosition = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/CharacterControllers/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : PhysicsObject
+{
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    [SerializeField] private SpriteRenderer spriteRenderer = null;
+    [SerializeField] private Animator animator = null;
+    [SerializeField] private Rigidbody2D playerRb = null;
+
+    private void Awake()
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+        if (playerRb == null)
+            playerRb = GetComponent<Rigidbody2D>();
+    }
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown ("Jump") && grounded) {
+            velocity.y = jumpTakeOffSpeed;
+        } 
+        else if (Input.GetButtonUp ("Jump")) 
+        {
+            if (velocity.y > 0) {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+        
+        bool flipSprite = (spriteRenderer.flipX ? (move.x >= 0f) : (move.x < 0f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        //   animator.SetBool ("grounded", grounded);
+        // animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+        if (targetVelocity == Vector2.zero)
+            animator.SetBool("WalkOn", false);
+        else
+            animator.SetBool("WalkOn", true);
+        playerRb.velocity = targetVelocity;
+    }
+}
