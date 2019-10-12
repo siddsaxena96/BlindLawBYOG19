e81d3a730eb484cfda45f207c2931b946ca493cd
sughoshpandey <sughoshpandey@gmail.com>
2019-10-13 04:06:56 +0800

    CourtElement

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Core.Events;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour, IEventListener
+{
+     public enum CourtEvents
+    {
+        Objection,
+        Sherlock,
+        CorrectOption
+    }
+
+    public Transform objectionPanel = null;
+    public Transform objectionBar = null;
+    private Animator objectionAnim = null;
+    private readonly string objectiveAnimString = "objectiveAnimString";
+
+    public Transform sherlockPanel = null;
+    public Text sherlockText = null;
+    public Animator sherlockAnim = null;
+
+    public Transform correctOptionPanel = null;
+    public Transform[] correctOptionButtons = null;
+    
+    
+
+    [SerializeField] private Core.Events.Event OnObjectionRaised;
+
+    public void OnEventRaised()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEventRaisedWithParameters(List<object> parameters)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnObjectionRaised?.RegisterListener(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
