0ea59d84d4fce2fbfc5732a4746244d2d9ed227c
Siddharth Saxena <sidd.saxena96@gmail.com>
2019-10-13 23:53:42 +0530

    +Multiple changes, game complete

--- Assets/anyclick.cs
+++ Assets/anyclick.cs
@@ -6,15 +6,14 @@ using UnityEngine.SceneManagement;
 public class anyclick : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+
     // Update is called once per frame
     public string sceneName = null;
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
         {
             SceneManager.LoadScene(sceneName);
         }
-        
-    }
+    }    
 }
